77875d1dbe5b2fa08c3bf2c2611f6e08d8886589
Karo Yousefi <karoyousefi.80@gmail.com>
2024-10-15 15:22:48 +0330

    First project is done

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { accordionData } from "../data/index";
+
+const Accordion = () => {
+	const [selected, setSelected] = useState(false); // For opening / clsoing one item at a time (if multipe is not active)
+	const [isMutli, setIsMulti] = useState(false); // For activating / deactivating multiple selection
+	const [mutli, setMulti] = useState([]); // An array of all the selected items for when the mulitple selection is active
+
+	const handleSelection = (id) => { // If multiple is not active, By clicking each item this funciton will run.
+		setSelected(selected === id ? null : id) // Check if the the item the user is selecting is already open or not, If it is open, it will be closed.
+	}
+
+	const handleMultiSelection = (id) => { // If multiple is active, By clicking each item this funciton will run. This funciton addes the id of the item to the mutli array
+		let copyMulti = [...mutli]; 
+		if (copyMulti.includes(id)) { // Alread open
+			copyMulti.splice(copyMulti.indexOf(id), 1); // Removing the id form the open array
+		}
+		else {
+			copyMulti.push(id); // Adding the id of the item to the open array
+		}
+		setMulti(copyMulti); // Updating the actual array
+
+	}
+	return (
+		<div className="flex flex-col gap-5 items-center">
+			<button
+				className="w-40 bg-red-400 rounded-md"
+				onClick={() => setIsMulti(!isMutli)}>
+				{isMutli ? "disable multi" : "enable multi"}
+			</button>
+			{
+				accordionData ? 
+				accordionData.map((item) => {
+					return (
+						<div className="w-[600px] flex flex-col gap-2" key={item.id}>
+							<div
+								className="w-full flex justify-between px-3 py-2 bg-sky-400 rounded-md cursor-pointer hover:bg-sky-300 transition-all select-none"
+								onClick={
+									isMutli ?
+									() => handleMultiSelection(item.id) : // If multiple is enabled, The id of the clicked item will be sent to handleMultiSelection function
+									() => handleSelection(item.id) // If multiple is not enabled, The id of the clicked item will be sent to handleSelection function
+								}>
+								<h1 className="text-3xl font-Montserrat font-semibold">{item.title}</h1>
+								<span className="text-3xl font-Montserrat font-semibold">+</span>
+							</div>
+							<div className="w-full">
+								{
+									isMutli ? // If mutli is selected
+										mutli.indexOf(item.id) !== -1 && ( // If item exists in the selected array
+											<p className="text-2xl font-Montserrat py-4">{item.text}</p>
+										) :
+										selected === item.id && ( // Single select
+											<p className="text-2xl font-Montserrat py-4">{item.text}</p>
+										)
+								}
+							</div>
+						</div>
+					)
+				}):
+				<p>Data not found!</p> // In case the data is not available
+			}
+		</div>
+	)
+}
+
+export default Accordion;
