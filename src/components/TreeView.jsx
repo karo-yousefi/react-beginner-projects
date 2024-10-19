573e2298c9ec1aeaba26c6a140655846b5e58966
Karo Yousefi <karoyousefi.80@gmail.com>
2024-10-19 12:23:12 +0330

    Updating project 6

--- src/components/TreeView.jsx
+++ src/components/TreeView.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 
-const TreeView = ({ data }) => {
+const TreeView = ({ data }) => { // The main component, This is a container for all the list and items
 	return (
 		<div className="w-full h-[100vh] flex justify-start">
 			<div className="w-[320px] h-full bg-sky-400">
@@ -11,7 +11,7 @@ const TreeView = ({ data }) => {
 	)
 }
 
-const MenuList = ({ list }) => {
+const MenuList = ({ list }) => { // Handling the array of objects in the data. It renders a list of items. Each item renders by ItemList component
 	return (
 		<ul className="px-5 py-3">
 			{
@@ -27,32 +27,31 @@ const MenuList = ({ list }) => {
 	)
 }
 
-const ItemList = ({ item }) => {
+const ItemList = ({ item }) => { // Rendering items
 
-	const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+	const [displayCurrentChildren, setDisplayCurrentChildren] = useState({}); // Tracking expansion / collapse of each item (if they have children)
 
 	const handleExpand = (item) => {
-		setDisplayCurrentChildren({...displayCurrentChildren, [item] : !displayCurrentChildren[item]})
+		setDisplayCurrentChildren({...displayCurrentChildren, [item] : !displayCurrentChildren[item]}) // Toggling between open and closed
 	}
-
 
 	return (
 		<li className="mt-2">
 				<div className="flex gap-4">
 					{
-						item && item.children && item.children.length ?
+						item && item.children ? // Ensuring the item is exists before checking if its children is falsy (undefined) or truthy 
 						<span className="cursor-pointer font-Montserrat text-3xl font-bold select-none hover:text-white transition-all" onClick={() => handleExpand(item.label)}>
 							{
-								displayCurrentChildren[item.label] ? "-" : "+"
+								displayCurrentChildren[item.label] ? "-" : "+" // Changint the text of the span based on if the item is open or closed (using label as key)
 							}
 						</span>:
-						null
+						null // If not item or no children in item do nothing
 					}
 					<p className="cursor-pointer font-Montserrat text-2xl select-none hover:font-semibold transition-all">{item.label}</p>
 				</div>
 				{
-					item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ?
-					<MenuList list={item.children}/> :
+					item && item.children && displayCurrentChildren[item.label] ? // Cheking each item for children, If there are children, The MenuList component will run again
+					<MenuList list={item.children}/> : // Calling the MenuList with the new children (recursion)]
 					null
 				}
 		</li>
