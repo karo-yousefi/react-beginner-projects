af090fb48a075961845678f5a1dccb62a30826df
Karo Yousefi <karoyousefi.80@gmail.com>
2024-10-26 11:52:06 +0330

    Updated project 13

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,21 +1,20 @@
 import { useEffect, useState } from "react";
 
-
 const TicTacToe = () => {
 
-	const [sqaureValues, setSqaureValues] = useState(Array(9).fill(""));
-	const [isXTurn, setISXTurn] = useState(true);
-	const [winner, setWinner] = useState("");
+	const [sqaureValues, setSqaureValues] = useState(Array(9).fill(""));  // This state is used to create the squares (line 74), Also to save each square's content (X or O)
+	const [isXTurn, setISXTurn] = useState(true); // true: X truns , false: O turns
+	const [winner, setWinner] = useState(null); // X: X is winner, O: O is winner, D: Draw (All the squares are full and no X or O), null: The game not finished yet
 
 
 	const updateValue = (index) => {
-		const copy = [...sqaureValues];
+		const copy = [...sqaureValues]; // You can not update a state directly, That's why we are manking a copy, Chnage it and then update the state with the copy using its setter function
 
-		if (checkForWinner(copy) || copy[index]) {
+		if (checkForWinner(copy) || copy[index]) { // If checkForWinner is null  or  the selected square is already full, do nothing
 			return;
 		};
 
-		copy[index] = isXTurn ? "X" : "O";
+		copy[index] = isXTurn ? "X" : "O"; // Updating the vlaue based on who turns it is
 
 		setSqaureValues(copy);
 		setISXTurn(!isXTurn);
@@ -36,13 +35,13 @@ const TicTacToe = () => {
 		];
 
 		for (let i=0; i<winningPatterns.length; i++) {
-			const [x,y,z] = winningPatterns[i];
+			const [x, y, z] = winningPatterns[i]; // x, y and z are the indexs of a winner pattern
 
-			if (sqaures[x] && sqaures[x] === sqaures[y] && sqaures[x] === sqaures[z]) {
-				return sqaures[x];
+			if (sqaures[x] && sqaures[x] === sqaures[y] && sqaures[x] === sqaures[z]) { // If values of the squares with the x, y and z index are the same (win condition)
+				return sqaures[x]; // Return the value (The winner)
 			}
 		}
-		return null;
+		return null; // No winner
 	}
 
 	const restart = () => {
@@ -50,14 +49,13 @@ const TicTacToe = () => {
 		setSqaureValues(Array(9).fill(""));
 	}
 
-	useEffect(() => {
-		if (!checkForWinner(sqaureValues) && sqaureValues.every((item) => item !== "")) {
+	useEffect(() => { // Updating the winner state whenever the turn changes
+		if (!checkForWinner(sqaureValues) && sqaureValues.every((item) => item !== "")) { // If there is not a winner (X or D) and all the squares are full (Draw condition)
 			setWinner("D");
 		}
 		else {
-			setWinner(checkForWinner(sqaureValues));
+			setWinner(checkForWinner(sqaureValues)); // Updating the winner state if there is a winner
 		}
-
 	}, [isXTurn])
 
 
@@ -65,7 +63,7 @@ const TicTacToe = () => {
 		<div className="w-full h-[100vh] flex justify-center items-center">
 			<div className="w-full h-[100vh] flex flex-col justify-center items-center gap-5">
 				{
-					isXTurn ? 
+					isXTurn ? // Updaing the the text based on the turn
 					<p className="text-2xl">X turns</p> :
 					<p className="text-2xl">O turns</p>
 				}
@@ -88,14 +86,15 @@ const TicTacToe = () => {
 								null
 				}
 				<button
-					className={`px-4 py-2 text-xl text-white bg-sky-700 hover:bg-sky-500 rounded-lg transition-all ${isFinished ? "" : "opacity-0"}`}
-					onClick={() => restart()}>Restart</button> :
+					className={`px-4 py-2 text-xl text-white bg-sky-700 hover:bg-sky-500 rounded-lg transition-all ${winner === null ? "opacity-0" : "opacity-100"}`}
+					onClick={() => restart()}>Restart</button>
 			</div>
 		</div>
 	)
 }
 
 export default TicTacToe
+
 
 const Sqaure = ({ value, handleClick }) => {
 	return (
