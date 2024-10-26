33e4cd758dab2a3af93283c1310a5ea796f5f8e7
Karo Yousefi <karoyousefi.80@gmail.com>
2024-10-26 13:55:51 +0330

    Updated project 14

--- src/components/FeatureFlag.jsx
+++ src/components/FeatureFlag.jsx
@@ -14,18 +14,83 @@ const FeatureFlag = () => {
 				onChange={() => toggleActive("accordion")}
 				/>
 				
-				<input
+			<input
 				type="checkbox"
 				checked={active["randomColorGen"]}
 				onChange={() => toggleActive("randomColorGen")}
 				/>
 				
-				<input
+			<input
 				type="checkbox"
 				checked={active["starRating"]}
 				onChange={() => toggleActive("starRating")}
 				/>
 				
+			<input
+				type="checkbox"
+				checked={active["imageSlider"]}
+				onChange={() => toggleActive("imageSlider")}
+				/>
+				
+			<input
+				type="checkbox"
+				checked={active["loadMore"]}
+				onChange={() => toggleActive("loadMore")}
+				/>
+				
+			<input
+				type="checkbox"
+				checked={active["treeView"]}
+				onChange={() => toggleActive("treeView")}
+				/>
+
+			<input
+				type="checkbox"
+				checked={active["QRGenerator"]}
+				onChange={() => toggleActive("QRGenerator")}
+				/>
+				
+			<input
+				type="checkbox"
+				checked={active["themeSwitch"]}
+				onChange={() => toggleActive("themeSwitch")}
+				/>
+				
+			<input
+				type="checkbox"
+				checked={active["scrollIndicator"]}
+				onChange={() => toggleActive("scrollIndicator")}
+				/>
+		
+			<input
+				type="checkbox"
+				checked={active["tabs"]}
+				onChange={() => toggleActive("tabs")}
+				/>
+				
+			<input
+				type="checkbox"
+				checked={active["customModal"]}
+				onChange={() => toggleActive("customModal")}
+				/>
+				
+			<input
+				type="checkbox"
+				checked={active["githubProfileFinder"]}
+				onChange={() => toggleActive("githubProfileFinder")}
+				/>
+
+			<input
+				type="checkbox"
+				checked={active["searchAutoCompelete"]}
+				onChange={() => toggleActive("searchAutoCompelete")}
+				/>
+
+			<input
+				type="checkbox"
+				checked={active["ticTacToe"]}
+				onChange={() => toggleActive("ticTacToe")}
+				/>
 		</div>
 	)
 }
